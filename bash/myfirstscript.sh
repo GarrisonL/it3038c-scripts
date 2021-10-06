a87fe9dcb78f9a325bbd63a7d0ee4753afbee077
GarrisonL <lee2gc@mail.uc.edu>
2021-10-06 00:52:10 -0400

    Lab 2

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is a script that outputs IP addresses and hostname
+greeting='This is a script. Hello!'
+echo $greeting, thanks for joining us!
+echo 'greeting, thanjs for joining us. You owe me $20'
+echo "$greeting, thanks for joining us!"
+echo "$greeting, you owe me \$20"
+
+
